335dcb9ce9d636e6eeb74529f51fa109de012d49
Rafael B. Tauil <rafael@tauil.com.br>
2011-07-11 22:44:09 -0300

    Improved flash messages, added avatar to owner field on task#show and added description label to task#show, ticket #5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def flash_messages
+    flash.collect do |key, msg|
+      content_tag :p, msg, :id => key, :class => "flash_messages"
+    end.join.html_safe
+  end
+
   def avatar_url(user)
     if user.url_image.present?
       user.url_image
