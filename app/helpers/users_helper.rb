bb36a52f347fd4ad17e45aafd5eb09857c968591
Luciano Sousa <ls@lucianosousa.net>
2011-07-24 15:08:59 -0300

    refactoring in avatar helper

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,15 @@
+#encoding: utf-8
+module UsersHelper
+  def show_user_avatar(user)
+   image_tag avatar_url(user), :class => "avatar icon" unless user.nil?
+  end
+
+  def avatar_url(user)
+    if user.url_image.present?
+      user.url_image
+    else
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    end
+  end
+end
