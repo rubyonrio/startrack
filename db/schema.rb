bad9e74d3aee5fd76802aac1fa9a32b0d6170867
Rafael B. Tauil <rafael@tauil.com.br>
2011-07-29 10:31:36 -0300

    #37 Fixed task description on task#show

--- db/schema.rb
+++ db/schema.rb
@@ -71,8 +71,8 @@ ActiveRecord::Schema.define(:version => 20110729021716) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "email",                                              :null => false
+    t.string   "encrypted_password",   :limit => 128,                :null => false
     t.string   "reset_password_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                       :default => 0
