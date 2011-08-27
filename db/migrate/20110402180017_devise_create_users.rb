b66aab094e4f5e4bbbfa29c1c8bd1801df803f93
Vinícius Sales <vinicius.sbs@gmail.com>
2011-08-26 22:33:32 -0300

    Old migration scripts rewritten according to the new ActiveRecord (version 3.1) pattern.

--- db/migrate/20110402180017_devise_create_users.rb
+++ db/migrate/20110402180017_devise_create_users.rb
@@ -1,18 +1,16 @@
 class DeviseCreateUsers < ActiveRecord::Migration
-  def self.up
-    create_table(:users) do |t|
+  def change
+    create_table :users do |t|
       t.string :name
       t.database_authenticatable :null => false
       t.recoverable
       t.rememberable
       t.trackable
       t.string :url_image
-
       # t.encryptable
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
-
 
       t.timestamps
     end
@@ -24,9 +22,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
-  end
-
-  def self.down
-    drop_table :users
   end
 end
