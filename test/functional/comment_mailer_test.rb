cf82c95d8e08917f181293efce0c3094c72997ac
Rafael B. Tauil <rafael@tauil.com.br>
2011-09-06 01:18:43 -0300

    Writing tests to Comment Mailer

--- test/functional/comment_mailer_test.rb
+++ test/functional/comment_mailer_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CommentMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  tests CommentMailer
+  test "comment notification" do
+    @expected.from    = 'me@example.com'
+    @expected.to      = 'friend@example.com'
+    @expected.subject = "[Projeto teste] New comment on #28 - Escrever testes"
+    #@expected.body    = read_fixture('body')
+    @expected.date    = Time.now
+ 
+    assert_equal @expected.encoded, CommentMailer.comment_notification(@watcher, @comment).encoded
+  end
 end
