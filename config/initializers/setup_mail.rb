e841f351e10332bfd02e010baf328f1ede0e0edf
Rafael B. Tauil <rafael@tauil.com.br>
2011-09-06 01:09:56 -0300

    Fixed default mail setup info

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {  
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "yourdomain.com.br",
-  :user_name            => "user@yourdomain.com.br",
+  :domain               => "example.com.br",
+  :user_name            => "user@example.com.br",
   :password             => "password",
   :authentication       => :plain,
   :enable_starttls_auto => true
