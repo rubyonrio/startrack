451e6a4cfedf48fccff8966e7a2589be95d75bbf
Rafael B. Tauil <rafael@tauil.com.br>
2011-09-07 18:52:11 -0300

    Fixing mailer tests

--- test/functional/comment_mailer_test.rb
+++ test/functional/comment_mailer_test.rb
@@ -2,11 +2,18 @@ require 'test_helper'
 
 class CommentMailerTest < ActionMailer::TestCase
   tests CommentMailer
+
+  def setup
+    @comment = comments(:one)
+    @task = tasks(:one)
+    @watcher = @task.watchers.first
+  end
+
   test "comment notification" do
     @expected.from    = 'me@example.com'
     @expected.to      = 'friend@example.com'
     @expected.subject = "[Projeto teste] New comment on #28 - Escrever testes"
-    #@expected.body    = read_fixture('body')
+    @expected.body    = read_fixture('body')
     @expected.date    = Time.now
  
     assert_equal @expected.encoded, CommentMailer.comment_notification(@watcher, @comment).encoded
