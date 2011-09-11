e7e55a10ff69a34a3477fb1299c337b4799a1537
Rodrigo Pinto <rodrigopqn@gmail.com>
2011-09-11 20:05:24 -0300

    remove unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :default_page_title
-
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
       projects_url
     else
       root_url
-    end
-  end
-
-  def default_page_title
-    load_page_title(nil)
-  end
-
-  def load_page_title(local_info)
-    if local_info.nil?
-      @page_title = "Startrack - Agile based tracker"
-    else
-      @page_title = "#{local_info} - Startrack - Agile based tracker"
     end
   end
 end
