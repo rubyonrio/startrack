d434dd946f4bf152d622377ec6500744895f0643
Rafael B. Tauil <rafael@tauil.com.br>
2011-09-11 14:54:55 -0300

    Updated SMTP setting with rubyonrio.org domain

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {  
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "example.com.br",
-  :user_name            => "user@example.com.br",
-  :password             => "password",
+  :domain               => "rubyonrio.org",
+  :user_name            => "startrack@rubyonrio.org",
+  :password             => "star!track.",
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
