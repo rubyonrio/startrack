2677cf991fae55957a3f132ad355f5e6b3a01ad3
Luciano Sousa <ls@lucianosousa.net>
2011-09-17 20:10:30 -0300

    update in project controller for use inherited resources

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,13 +3,8 @@ class ProjectsController < InheritedResources::Base
   respond_to :html
   actions :all
 
-  def index
-    @projects = current_user.projects
-  end
-
   def show
     show! do
-      @project = Project.find(params[:id])
       @tasks = @project.tasks.all
       @task_todo = @project.tasks.todo
       @task_scheduled = @project.tasks.scheduled
@@ -18,35 +13,9 @@ class ProjectsController < InheritedResources::Base
     end
   end
 
-  def new
-    @project = current_user.projects.build
-  end
-
-  def edit
-    @project = current_user.projects.find(params[:id])
-  end
-
-  def create
-    @project = current_user.projects.build(params[:project])
-    if @project.save
-      redirect_to @project, notice: 'Project was successfully created.'
-    else
-      render action: "new"
-    end
-  end
-
-  def update
-    @project = current_user.projects.find(params[:id])
-    if @project.update_attributes(params[:project])
-      redirect_to @project, notice: 'Project was successfully updated.'
-    else
-      render action: "edit"
-    end
-  end
-
-  def destroy
-    @project = current_user.projects.find(params[:id])
-    @project.destroy
-    redirect_to projects_url
+  protected
+  def begin_of_association
+    @project = current_user.projects.where(id: params[:id]).first
+    @projects = current_user.projects
   end
 end
