6a0005faf461fe02e50c8a73bb621ff0a7bd01fc
Luciano Sousa <ls@lucianosousa.net>
2011-09-23 17:00:00 -0300

    fix comment broken if not fill in description

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,28 +4,19 @@ class CommentsController < ApplicationController
     @task = Task.find(params[:task_id])
     @comment = @task.comments.build(params[:comment])
     @comment.user = current_user
-
-    respond_to do |format|
-      if @comment.save
-        @task.watchers << current_user unless @task.watchers.include?(current_user)
-        notify_comment(@comment)
-        format.html { redirect_to @task, notice: 'Comment was successfully created.' }
-        format.json { render json: [:task, @comment], status: :created, location: @comment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: [:task, @comment].errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      @task.watchers << current_user unless @task.watchers.include?(current_user)
+      notify_comment(@comment)
+      redirect_to @task, notice: 'Comment was successfully created.'
+    else
+     redirect_to @task, alert: 'Comment was not created.'
     end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to task_url(@comment.task), alert: 'Comment was successfully deleted.' }
-      format.json { head :ok }
-    end
+    redirect_to task_url(@comment.task), alert: 'Comment was successfully deleted.'
   end
 
   def notify_comment(comment)
