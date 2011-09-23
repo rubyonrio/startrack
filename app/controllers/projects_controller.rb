bf754acbcb354b0b7ce28a7459b747ff448c602b
Luciano Sousa <ls@lucianosousa.net>
2011-09-23 16:19:50 -0300

    rewrite project controller for not use inherited resources

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,24 +1,49 @@
-class ProjectsController < InheritedResources::Base
+class ProjectsController < ApplicationController
   before_filter :authenticate_user!
-  respond_to :html
-  actions :all
 
   def index
+    @projects = current_user.projects
   end
 
   def show
-    show! do
-      @tasks = @project.tasks.all
-      @task_todo = @project.tasks.todo
-      @task_scheduled = @project.tasks.scheduled
-      @task_current = @project.tasks.current
-      @task_done = @project.tasks.done
+    @project = Project.find(params[:id])
+    @tasks = @project.tasks.all
+    @task_todo = @project.tasks.todo
+    @task_scheduled = @project.tasks.scheduled
+    @task_current = @project.tasks.current
+    @task_done = @project.tasks.done
+  end
+
+  def new
+    @project = current_user.projects.build
+  end
+
+  def edit
+    @project = current_user.projects.find(params[:id])
+  end
+
+  def create
+    @project = current_user.projects.build(params[:project])
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render action: "new"
     end
   end
 
-  protected
-  def begin_of_association_chain
-    @project = current_user.projects.where(id: params[:id]).first
-    @projects = current_user.projects if current_user.projects.any?
+  def update
+    @project = current_user.projects.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @project = current_user.projects.find(params[:id])
+    @project.destroy
+    redirect_to projects_url
   end
 end
+
--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -4,12 +4,35 @@ class ProjectsControllerTest < ActionController::TestCase
 
   setup do
     @project = projects(:first_journey)
-    sign_in users(:spok)
+    @current_user = users(:spok)
+    sign_in @current_user
   end
 
-  test "should respond success" do
-    get :show, id: @project.to_param
+  test "should list all projects of the user" do
+    get :index
+    assert_equal assigns(:projects), @current_user.projects
     assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_not_nil assigns(:project)
+    assert_response :success
+  end
+
+  test "should create a new project with relationship with user" do
+    assert_difference('Project.count') do
+      post :create, project: {name: "Skatepark", user_ids: [@current_user.id]}
+    end
+
+    assert_redirected_to project_path(assigns(:project))
+    assert_equal "Project was successfully created.", flash[:notice]
+  end
+
+
+  test "should not create a new project if don't have the requirements" do
+    post :create, project: {name: "", user_ids: [@current_user.id]}
+      assert_nil flash[:notice]
   end
 
   test "should show all related kind of task for a project" do
@@ -19,5 +42,28 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:task_scheduled)
     assert_not_nil assigns(:task_current)
     assert_not_nil assigns(:task_done)
+  end
+
+  test "should get edit" do
+    get :edit, id: @project.id
+    assert_not_nil assigns(:project)
+    assert_response :success
+  end
+
+  test "should update a project" do
+    put :update, id: @project.id
+    assert_equal 'Project was successfully updated.', flash[:notice]
+    assert_redirected_to project_path(assigns(:project))
+  end
+
+  test "should not update a project if don't have the requirements" do
+    put :update, id: @project.id, project: {name: ''}
+    assert_nil flash[:notice]
+  end
+
+  test "should delete a project" do
+    assert_difference('Project.count', -1) do
+      delete :destroy, id: @project.id
+    end
   end
 end
