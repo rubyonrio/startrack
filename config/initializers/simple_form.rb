b7720c5b766d9da0cc06ea7bc9e3e8464c6b98ed
Cairo Noleto <caironoleto@gmail.com>
2011-10-10 01:02:11 -0300

    Adding new simple form initializer and adding twitter bootstrap wrappers

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,20 +1,52 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  # Components used by the form builder to generate a complete input. You can remove
-  # any of them, change the order, or even add your own components to the stack.
-  # config.components = [ :placeholder, :label_input, :hint, :error ]
+  # Wrappers are used by the form builder to generate a complete input.
+  # You can remove any component from the wrapper, change the order or even
+  # add your own to the stack. The options given to the wrappers method
+  # are used to wrap the whole input (if any exists).
+  config.wrappers :inline, :class => 'clearfix', :error_class => :error do |b|
+    b.use :placeholder
+    b.use :label
+    b.use :tag => 'div', :class => 'input' do |ba|
+      ba.use :input
+      ba.use :error, :tag => :span, :class => :'help-inline'
+      ba.use :hint,  :tag => :span, :class => :'help-block'
+    end
+  end
 
-  # Default tag used on hints.
-  # config.hint_tag = :span
+  config.wrappers :stacked, :class => "clearfix", :error_class => :error do |b|
+    b.use :placeholder
+    b.use :label
+    b.use :hint,  :tag => :span, :class => :'help-block'
+    b.use :tag => 'div', :class => 'input' do |input|
+      input.use :input
+      input.use :error, :tag => :span, :class => :'help-inline'
+    end
+  end
 
-  # CSS class to add to all hint tags.
-  # config.hint_class = :hint
+  config.wrappers :prepend, :class => "clearfix", :error_class => :error do |b|
+    b.use :placeholder
+    b.use :label
+    b.use :hint,  :tag => :span, :class => :'help-block'
+    b.use :tag => 'div', :class => 'input' do |input|
+      input.use :tag => 'div', :class => 'input-prepend' do |prepend|
+        prepend.use :input
+      end
+      input.use :error, :tag => :span, :class => :'help-inline'
+    end
+  end
 
-  # CSS class used on errors.
-  # config.error_class = :error
-
-  # Default tag used on errors.
-  # config.error_tag = :span
+  config.wrappers :append, :class => "clearfix", :error_class => :error do |b|
+    b.use :placeholder
+    b.use :label
+    b.use :hint,  :tag => :span, :class => :'help-block'
+    b.use :tag => 'div', :class => 'input' do |input|
+      input.use :tag => 'div', :class => 'input-append' do |append|
+        append.use :input
+      end
+      input.use :error, :tag => :span, :class => :'help-inline'
+    end
+  end
 
   # Method used to tidy up errors.
   # config.error_method = :first
@@ -28,26 +60,17 @@ SimpleForm.setup do |config|
   # ID to add for error notification helper.
   # config.error_notification_id = nil
 
-  # You can wrap all inputs in a pre-defined tag.
-  # config.wrapper_tag = :div
+  # Series of attempts to detect a default label method for collection.
+  # config.collection_label_methods = [ :to_label, :name, :title, :to_s ]
 
-  # CSS class to add to all wrapper tags.
-  # config.wrapper_class = :input
-
-  # CSS class to add to the wrapper if the field has errors.
-  # config.wrapper_error_class = :field_with_errors
+  # Series of attempts to detect a default value method for collection.
+  # config.collection_value_methods = [ :id, :to_s ]
 
   # You can wrap a collection of radio/check boxes in a pre-defined tag, defaulting to none.
   # config.collection_wrapper_tag = nil
 
   # You can wrap each item in a collection of radio/check boxes with a tag, defaulting to span.
   # config.item_wrapper_tag = :span
-
-  # Series of attempts to detect a default label method for collection.
-  # config.collection_label_methods = [ :to_label, :name, :title, :to_s ]
-
-  # Series of attempts to detect a default value method for collection.
-  # config.collection_value_methods = [ :id, :to_s ]
 
   # How the label text should be generated altogether with the required text.
   # config.label_text = lambda { |label, required| "#{required} #{label}" }
@@ -71,13 +94,13 @@ SimpleForm.setup do |config|
   # HTML5-aware browsers such as Chrome.
   # config.html5 = true
 
+  # Collection of methods to detect if a file type was given.
+  # config.file_methods = [ :mounted_as, :file?, :public_filename ]
+
   # Custom mappings for input types. This should be a hash containing a regexp
   # to match as key, and the input type that will be used when the field name
   # matches the regexp as value.
   # config.input_mappings = { /count/ => :integer }
-
-  # Collection of methods to detect if a file type was given.
-  # config.file_methods = [ :mounted_as, :file?, :public_filename ]
 
   # Default priority for time_zone inputs.
   # config.time_zone_priority = nil
@@ -90,4 +113,13 @@ SimpleForm.setup do |config|
 
   # When false, do not use translations for labels, hints or placeholders.
   # config.translate = true
+
+  # Automatically discover new inputs in Rails' autoload path.
+  # config.inputs_discovery = true
+
+  # Cache simple form inputs discovery
+  # config.cache_discovery = !Rails.env.development?
+
+  # Default class for buttons
+  # config.button_class = 'button'
 end
