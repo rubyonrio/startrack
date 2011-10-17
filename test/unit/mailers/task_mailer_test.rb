328e59a364bff95ba558e1cb0663508c819847c7
Rafael B. Tauil <rafael@tauil.com.br>
2011-10-17 01:03:04 -0200

    Detailed task changes but still need to get nested attributes names and fix add/rm watchers duplicate bug

--- test/unit/mailers/task_mailer_test.rb
+++ test/unit/mailers/task_mailer_test.rb
@@ -9,7 +9,13 @@ class TaskMailerTest < ActionMailer::TestCase
     @watchers_changes = {}
     @watchers_changes[:added] = [tasks(:room_galaxy).watchers.first]
     @watchers_changes[:removed] = [tasks(:room_galaxy).watchers.last]
-    @changes = tasks(:room_galaxy).type.name
+    @changes = {}
+    @changes[:name] = [tasks(:room_galaxy).name, "Dominate the galaxy"]
+    @changes[:description] = [tasks(:room_galaxy).description, "Annihilate the enemy"]
+    @changes[:responsible_id] = [tasks(:room_galaxy).responsible.name, "Spok"]
+    @changes[:type_id] = [tasks(:room_galaxy).type.name, "Feature"]
+    @changes[:status_id] = [tasks(:room_galaxy).status.name, "Scheduled"]
+    @changes[:estimate_id] = [tasks(:room_galaxy).estimate.name, "Longstanding"]
     @task_notification = TaskMailer.task_notification(@watcher, @task, @changes, @watchers_changes)
   end
 
