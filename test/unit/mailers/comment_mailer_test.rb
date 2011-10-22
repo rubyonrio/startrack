de420d70ce205ff4965e0e9233e5a56efed7dc38
Rafael B. Tauil <rafael@tauil.com.br>
2011-10-22 12:21:47 -0200

    Fixed get_changes_names bug when responsible nil and improved code

--- test/unit/mailers/comment_mailer_test.rb
+++ test/unit/mailers/comment_mailer_test.rb
@@ -5,7 +5,7 @@ class CommentMailerTest < ActionMailer::TestCase
 
   def setup
     @comment = comments(:one)
-    @watcher = tasks(:create_enterprise).watchers.first
+    @watcher = tasks(:room_galaxy).watchers.first
     @commment_notification = CommentMailer.comment_notification(@watcher, @comment)
   end
 
