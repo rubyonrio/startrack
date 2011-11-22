eca8ee16ec388a2e2025279cb2bcd0676f00d94c
Rafael B. Tauil <rafael@tauil.com.br>
2011-11-22 17:25:05 -0200

    Added who comment to mail subject

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,6 +1,6 @@
 class CommentMailer < ActionMailer::Base
   def comment_notification(watcher, comment)
     @watcher, @comment, @task = watcher, comment, comment.task
-    mail(:to => watcher.email, :subject => "[#{@task.project.name}] New comment on ##{@task.id} - #{@task.name}", :from => "startrack@example.com.br")
+    mail(:to => watcher.email, :subject => "[#{@task.project.name}] #{@comment.user.name} made a new comment on ##{@task.id} - #{@task.name}", :from => "startrack@example.com.br")
   end 
 end
--- test/unit/mailers/comment_mailer_test.rb
+++ test/unit/mailers/comment_mailer_test.rb
@@ -12,7 +12,7 @@ class CommentMailerTest < ActionMailer::TestCase
   test "comment notification" do
     assert_equal ["startrack@example.com.br"], @commment_notification.from
     assert_equal ["kirk@example.com"], @commment_notification.to
-    assert_equal "[First Journey] New comment on #587127017 - Create Enterprise", @commment_notification.subject
+    assert_equal "[First Journey] Spok made a new comment on #587127017 - Create Enterprise", @commment_notification.subject
     assert_equal "text/plain; charset=UTF-8", @commment_notification.content_type
     assert_equal read_fixture('comment_notification').join, @commment_notification.body.to_s
   end
