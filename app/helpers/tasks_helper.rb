7197665a981795d0c836a96a74c76a9b73324eaa
Rodrigo Pinto <rodrigopqn@gmail.com>
2011-12-18 12:57:42 -0200

    cleanup helper refactory in the future

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -7,12 +7,4 @@ module TasksHelper
   def show_task_description(task)
     task.description_html? ? task.description_html.html_safe : task.description
   end
-
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", :f => builder, :child_index => "new_#{association}")
-    end
-    link_to_function(name, "add_fields(\"#{association}\", \"#{escape_javascript(fields)}\")", :class => "button small add-image")
-  end
 end
