4aa9b88f3024839f1d248aaf57d690aa8230e41f
Rodrigo Pinto <rodrigopqn@gmail.com>
2011-12-18 13:52:51 -0200

    add coverage to action project#destroy

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -61,5 +61,30 @@ describe ProjectsController do
       it { should render_template(:edit) }
     end
   end
-  
+
+  describe "GET destroy" do
+    it_should_behave_like "authentication_required_action"
+    let(:project) { projects(:first_journey) }
+
+    def do_action
+      delete(:destroy, :id => project.id)
+    end
+
+    context "authenticated" do
+      before(:each) do
+        login!
+      end
+
+      it "should delete a comment" do
+        expect {
+          do_action
+        }.to change(Project, :count).by(-1)
+      end
+
+      it "should redirect to projects url" do
+        do_action
+        should redirect_to(projects_url)
+      end
+    end
+  end
 end
