b61120dd723efd80fcef2ad2b1911dea761c322a
Rodrigo Pinto <rodrigopqn@gmail.com>
2011-12-17 23:22:28 -0200

    add spec to show_task_description

--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -8,4 +8,19 @@ describe TasksHelper do
       helper.link_to_show_task(task).should == "<a href=\"/tasks/587127017\" class=\"todo bug\">#587127017 Bug - Create Enterprise</a>"
     end
   end
+
+  describe "show_task_description" do
+    context "when task is nil" do
+      it "does something" do
+        task.description_html = nil
+        helper.show_task_description(task).should == "Something cool here!"
+      end
+    end
+
+    context "when task is not nil" do
+      it "does something" do
+        helper.show_task_description(task).should == "<p>Something cool here!</p>"
+      end
+    end
+  end
 end
