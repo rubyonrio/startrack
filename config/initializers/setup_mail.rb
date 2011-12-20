59ca00b471b93fb2849482fb42419d64d3c3c723
Bruno Barros <bkether@gmail.com>
2011-12-20 12:41:30 -0200

    Commenting setup mail and adding config action mailer default url options

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "example.com.br",
-  :user_name            => "user@example.com.br",
-  :password             => "password",
-  :authentication       => :plain,
-  :enable_starttls_auto => true
-}
+# SMTP settings
+#
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :domain               => "example.com.br",
+#   :user_name            => "user@example.com.br",
+#   :password             => "password",
+#   :authentication       => :plain,
+#   :enable_starttls_auto => true
+# }
