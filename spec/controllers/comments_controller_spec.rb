51d2ac1835898d5ecbe7dbe31f453b23b520b0c6
Rafael B. Tauil <rafael@tauil.com.br>
2011-12-26 15:58:09 -0200

    Added comments tests for create action

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,9 +1,46 @@
 require 'spec_helper'
 
 describe CommentsController do
+
+  let(:task) { tasks(:create_enterprise) }
+  let(:comment) { comments(:one) }
+
+  describe "POST create" do
+    it_should_behave_like "authentication_required_action"
+
+    def do_action(attributes = {})
+      post(:create, :task_id => task.id, comment: attributes)
+    end
+
+    context "authenticated" do
+      before(:each) do
+        login!
+      end
+
+      context "valid attributes" do
+        before(:each) do
+          do_action( description: "Hidden Project")
+        end
+
+        it { should assign_to(:task)}
+        it { should redirect_to(assigns(:task)) }
+        it { should set_the_flash.to("Comment was successfully created.") }
+      end
+
+      context "invalid attributes" do
+        before(:each) do
+          do_action
+        end
+
+        it { should set_the_flash.to("Comment was not created.") }
+      end
+
+    end
+
+  end
+
   describe "GET destroy" do
     it_should_behave_like "authentication_required_action"
-    let(:comment) { comments(:one) }
 
     def do_action
       delete(:destroy, :id => comment.id)
