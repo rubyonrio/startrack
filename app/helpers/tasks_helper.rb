7644abc52636d0414e8321980c428aa7f34ac98c
Rafael B. Tauil <rafael@tauil.com.br>
2012-01-04 01:20:49 -0200

    Improved timer layout

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -11,9 +11,15 @@ module TasksHelper
 
   def link_to_time_track(project, task)
     if task.start_time.nil?
-      link_to("Start (#{task.duration_time}m)", start_project_task_path(project, task), :remote => true)
+      link_to start_project_task_path(project, task), :class => "timer", :remote => true do
+        image_tag("ico-timer.png", :class => "icon")+
+        "#{task.duration_time}m"
+      end
     else
-      link_to("Stop (#{task.duration_time}m)", stop_project_task_path(project, task), :remote => true)
+      link_to stop_project_task_path(project, task), :class => "timer active", :remote => true do
+        image_tag("ico-timer-active.png", :class => "icon")+
+        "#{task.duration_time}m"
+      end
     end
   end
 
