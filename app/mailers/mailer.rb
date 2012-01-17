8409ab1689cac2e925b9288433f6d66fe86fa724
Bruno Barros <bkether@gmail.com>
2012-01-17 21:00:45 -0200

    Removing comment about attachments.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,7 +2,6 @@ class Mailer < Devise::Mailer
   include Resque::Mailer
 
   def reset_password_instructions(record)
-    # attachments['logo.png'] = File.read('')
     devise_mail(record, :reset_password_instructions)
   end
 end
