e2d65766ce148c246ad5c3f99bf4ba6a73e15dd9
Bruno Barros <bkether@gmail.com>
2012-01-17 21:01:51 -0200

    Changing devise config mailer to DeviseResqueMailer.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,13 +1,15 @@
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
+  require 'devise_resque_mailer'
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
   config.mailer_sender = "please-change-me@config-initializers-devise.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
-  config.mailer = "::Mailer"
+  config.mailer = "DeviseResqueMailer"
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
