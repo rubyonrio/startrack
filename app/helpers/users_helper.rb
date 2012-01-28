c4ba01fa40b50e950f6dfd854c7f73b0ae2ffbfa
Rodrigo Pinto <rodrigopqn@gmail.com>
2012-01-28 18:46:21 -0200

    change how avatar is size comes to remove css class

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,19 @@
 #encoding: utf-8
 module UsersHelper
   def show_user_avatar(user)
-   image_tag avatar_url(user), :class => "avatar icon" unless user.nil?
+   image_tag avatar_url(user), title: "#{user.name}" unless user.nil?
   end
 
   private
-  def avatar_url(user)
-    if user.url_image?
-      user.url_image
-    else
-      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    def avatar_url(user)
+      if user.url_image?
+        user.url_image
+      else
+        "http://gravatar.com/avatar/#{gravatar_id(user)}?s=30"
+      end
     end
-  end
+
+    def gravatar_id(user)
+      Digest::MD5.hexdigest(user.email.downcase)
+    end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -7,13 +7,13 @@ describe UsersHelper do
     context "when user has url_image" do
       it "should return the url_image" do
         user.url_image = "http://someurl.com/with/an_image.png"
-        helper.show_user_avatar(user).should == "<img alt=\"An_image\" class=\"avatar icon\" src=\"http://someurl.com/with/an_image.png\" />"      
+        helper.show_user_avatar(user).should == "<img alt=\"An_image\" src=\"http://someurl.com/with/an_image.png\" title=\"Spok\" />"
       end            
     end
 
     context "when user has not url_image" do
       it "should return the gravatar url" do
-        helper.show_user_avatar(user).should == "<img alt=\"67f5a867ebf8980611ea483756290801\" class=\"avatar icon\" src=\"http://gravatar.com/avatar/67f5a867ebf8980611ea483756290801.png?s=48\" />"
+        helper.show_user_avatar(user).should == "<img alt=\"67f5a867ebf8980611ea483756290801?s=30\" src=\"http://gravatar.com/avatar/67f5a867ebf8980611ea483756290801?s=30\" title=\"Spok\" />"
       end      
     end
   end
