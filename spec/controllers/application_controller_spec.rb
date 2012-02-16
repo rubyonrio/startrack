952538c73c873286e0050f2ad26b19ef86602313
Rafael B. Tauil <rafael@tauil.com.br>
2012-02-16 17:55:17 -0200

    Removes whitespaces

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -10,18 +10,18 @@ describe ApplicationController do
   describe "After sigin-in" do
     context "resource is an User" do
       let(:user) { users(:spok) }
-      
+
       it "redirects to projects_url" do
         controller.after_sign_in_path_for(user).should == projects_url
-      end      
+      end
     end
-    
+
     # TODO: I should confirme, why test if the resource is User
     context "resource is not an User" do
       let(:task) { tasks(:create_enterprise) }
       it "redirects to root_url" do
         controller.after_sign_in_path_for(task).should == root_url
-      end      
+      end
     end
   end
 end
