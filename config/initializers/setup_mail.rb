620e60e29da9384f42154d7539f1ce664d061472
Bruno Barros <bkether@gmail.com>
2012-03-18 13:05:27 -0300

    Commenting setup mail, don't forget personal config here

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "example.com.br",
-  :user_name            => "bkether@gmail.com",
-  :password             => "7bo7ak1c4x9",
-  :authentication       => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :domain               => "example.com.br",
+#   :user_name            => "user@example.com.br",
+#   :password             => "password",
+#   :authentication       => :plain,
+#   :enable_starttls_auto => true
+# }
