4d80d8b02a57028b04de620b1995177518a9cace
Luciano Sousa <luciano@bonera.com.br>
2012-03-18 13:38:28 -0300

    improvements in resque mailer for comment mailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     if @comment.save
       @task.watchers << current_user unless @task.watchers.include?(current_user)
-      notify_comment(@comment)
+      CommentMailer.comment_notification(@comment.id).deliver!
       redirect_to project_task_path(project,@task), notice: 'Comment was successfully created.'
     else
       redirect_to project_task_path(project,@task), alert: 'Comment was not created.'
@@ -18,14 +18,6 @@ class CommentsController < ApplicationController
     @comment = task.comments.find(params[:id])
     @comment.destroy
     redirect_to project_task_path(project, @comment.task), alert: 'Comment was successfully deleted.'
-  end
-
-  def notify_comment(comment)
-    recipients = ""
-    unless @task.watchers.nil?
-      @task.watchers.map { |watcher| recipients << "#{watcher.email}," }
-      CommentMailer.comment_notification(recipients, comment).deliver
-    end
   end
   
   private
--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,9 +1,12 @@
 class CommentMailer < ActionMailer::Base
-  #include Resque::Mailer
+  include Resque::Mailer
 
-  def comment_notification(recipients, comment)
-    @comment, @task = comment, comment.task
+  def comment_notification(comment_id)
+    @comment = Comment.find comment_id
+    @task = @comment.task
+    recipients = ""
+    @task.watchers.map { |watcher| recipients << "#{watcher.email}," }
 
-    mail(:to => recipients, :subject => "[#{comment.task.project.name}] #{comment.user.name} made a new comment on ##{comment.task.id} - #{comment.task.name}", :from => "startrack@example.com.br")
+    mail(:to => recipients, :subject => "[#{@comment.task.project.name}] #{@comment.user.name} made a new comment on ##{@comment.task.id} - #{@comment.task.name}", :from => "startrack@example.com.br")
   end
 end
