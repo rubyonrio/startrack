00b0a1b6c88d357475de0f0a26c151e0f1bb6394
Anna Cruz <anna.cruz@gmail.com>
2018-03-23 18:09:31 -0300

    WIP: Migrating to rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
-Startrack::Application.routes.draw do
-
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  resources :projects do
+  resource :projects do
     resources :tasks, :except => [:index, :new] do
-      match "/change_status/:status_id" => "tasks#change_status", :as => :change_status, :on => :member
-      match 'start' => 'tasks#start', :as => "start", :on => :member
-      match 'stop' => 'tasks#stop', :as => "stop", :on => :member
+      match "change_status/:status_id" => 'tasks#change_status', :as => :change_status, :on => :member
+      match 'start' => 'tasks#start', :as => 'start', :on => :member
+      match 'stop' => 'tasks#stop', :as => 'stop', :on => :member
       resources :comments, :only => [:create, :destroy]
     end
   end
 
-  root :to => "projects#index"
+  root :to => 'projects#index'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 require 'simplecov'
 SimpleCov.start 'rails'
 
+require 'minitest/autorun'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
