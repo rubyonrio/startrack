84cec0e5de24fe7a33111efda6f56dcb0ea3d530
Anna Cruz <anna.cruz@gmail.com>
2018-04-04 12:29:51 -0300

    WIP: Fix tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe ApplicationController do
+describe ApplicationController, :type => :controller do
   controller do
     def after_sign_in_path_for(resource)
       super resource
