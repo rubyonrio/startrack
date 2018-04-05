a7c671c3e37fbb2aedb4bc02af135b3509a7229f
Anna Cruz <anna.cruz@gmail.com>
2018-04-05 16:02:54 -0300

    Fix application_controller_spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe ApplicationController do
   controller do
@@ -9,19 +9,19 @@ describe ApplicationController do
 
   describe "After sigin-in" do
     context "resource is an User" do
-      let(:user) { users(:spok) }
-      
+      let(:user) { User.find_by_name('Spok') }
+
       it "redirects to projects_url" do
-        controller.after_sign_in_path_for(user).should == projects_url
-      end      
+        expect(controller.after_sign_in_path_for(user)).to eq(projects_url)
+      end
     end
-    
+
     # TODO: I should confirme, why test if the resource is User
     context "resource is not an User" do
-      let(:task) { tasks(:create_enterprise) }
+      let(:task) { Task.find_by_name('Create enterprise') }
       it "redirects to root_url" do
-        controller.after_sign_in_path_for(task).should == root_url
-      end      
+        expect(controller.after_sign_in_path_for(task)).to eq(root_url)
+      end
     end
   end
 end
