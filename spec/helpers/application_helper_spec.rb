bbece1c051f5db595278f6d1669e2503a5e610c3
Anna Cruz <anna.cruz@gmail.com>
2018-04-05 16:02:08 -0300

    Update rspec and these configurations

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe ApplicationHelper do
   describe '#link_to_logout' do
--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe TasksHelper do
   let(:project) { projects(:first_journey) }
