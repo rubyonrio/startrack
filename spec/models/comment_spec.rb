65252d9be379505015abc5c9133a63ebc445593c
Anna Cruz <anna.cruz@gmail.com>
2018-05-12 09:29:06 -0300

    Fix comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Comment do
   it { should belong_to(:task) }
@@ -6,17 +6,17 @@ describe Comment do
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:task) }
   it { should validate_presence_of(:user) }
-  
+
   describe "to_format" do
     let(:comment) { Factory.build(:comment) }
 
     it "should call to_format before save" do
       comment = Comment.new
       comment.description = "I got some new issue to resolve!"
-      comment.user = users(:mccoy) 
-      comment.task = tasks(:room_galaxy)
+      comment.user = User.find_by_name("Leonard McCoy")
+      comment.task = Task.find_by_name("Room the galaxy")
       comment.save
-      comment.description.should == "<p>I got some new issue to resolve!</p>"
+      expect(comment.description).to eq("<p>I got some new issue to resolve!</p>")
     end
   end
 end
