f609eb6205df6abad1575ecb7b908b3978c439dd
Anna Cruz <anna.cruz@gmail.com>
2018-05-11 22:57:33 -0300

    Fix task_helper_spec

--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 describe TasksHelper do
-  let(:project) { projects(:first_journey) }
-  let(:task) { tasks(:create_enterprise) }
+  let(:task) { Task.find_by_name("Create Enterprise") }
+  let(:project) { Project.find_by_name("First Journey") }
 
   describe "link_to_show_task" do
     it "should return a link ti show task" do
-      helper.link_to_show_task(project, task).should == "<a href=\"#{project_task_path(project,task)}\" class=\"todo bug\">##{task.id} #{task.type.name} - #{task.name}</a>"
+      expect(helper.link_to_show_task(project, task)).to eq("<a class=\"todo bug\" href=\"#{project_task_path(project, task)}\">##{task.id} #{task.type.name} - #{task.name}</a>")
     end
   end
 
   describe "time tracker" do
     it "should return a link to start the timer" do
-      helper.link_to_time_track(project, task).should == "<a href=\"#{start_project_task_path(project,task)}\" class=\"timer\" data-remote=\"true\"><img alt=\"Ico-timer\" class=\"icon\" src=\"/assets/ico-timer.png\" />#{task.duration_time}m</a>"
+      expect(helper.link_to_time_track(project, task)).to eq("<a class=\"timer\" data-remote=\"true\" href=\"#{start_project_task_path(project,task)}\"><img class=\"icon\" src=\"/assets/ico-timer-318ddd204154ee04cd5e7a1bf3e33654dc6235d0c4149f9298335d57e902f6a1.png\" alt=\"Ico timer\" />#{task.duration_time}m</a>")
     end
 
     it "should return a link to stop the timer" do
       task.start_time = Time.now
-      helper.link_to_time_track(project, task).should == "<a href=\"#{stop_project_task_path(project,task)}\" class=\"timer active\" data-remote=\"true\"><img alt=\"Ico-timer-active\" class=\"icon\" src=\"/assets/ico-timer-active.png\" />#{task.duration_time}m</a>"
+      expect(helper.link_to_time_track(project, task)).to eq("<a class=\"timer active\" data-remote=\"true\" href=\"#{stop_project_task_path(project,task)}\"><img class=\"icon\" src=\"/assets/ico-timer-active-c5cdaef9ee353a2216c31c4d46eb28ad4ee097e9fdb7ba42bf9fcf3ede53651d.png\" alt=\"Ico timer active\" />#{task.duration_time}m</a>")
     end
   end
 end
