17802a396c88c4696b49bd30c14398518810031c
Anna Cruz <anna.cruz@gmail.com>
2018-05-12 09:38:58 -0300

    Fix task_spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,8 +1,8 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Task do
 
-  let(:task) { tasks(:create_enterprise) }
+  let(:task) { Task.find_by_name("Create Enterprise") }
 
   describe "Relationships" do
     it { should belong_to(:project) }
@@ -24,40 +24,40 @@ describe Task do
   end
 
   it "should show 'No responsible yet' if do not have a responsible" do
-    task.show_responsible_name.should == 'No responsible yet'
+    expect(task.show_responsible_name).to eq('No responsible yet')
   end
 
   it "should show a estimative with 'show_estimate_name'" do
-    task.show_estimate_name.should == 'Fast'
+    expect(task.show_estimate_name).to eq('Fast')
   end
 
   it "should format task description with markdown" do
     task.description = '* hello world'
-    task.normalize_description.should == "<ul>\n\t<li>hello world</li>\n</ul>"
+    expect(task.normalize_description).to eq("<ul>\n\t<li>hello world</li>\n</ul>")
   end
 
   it "should get watchers changes" do
-    @watchers_changes = task.get_watchers_changes([users(:mccoy).id])
-    @watchers_changes[:added].should == [users(:mccoy).id]
-    @watchers_changes[:removed].should == [users(:kirk).id]
+    @watchers_changes = task.get_watchers_changes([User.find_by_name("Leonard McCoy").id])
+    expect(@watchers_changes[:added]).to eq([User.find_by_name("Leonard McCoy").id])
+    expect(@watchers_changes[:removed]).to eq([User.find_by_name("Commander Kirk").id])
   end
 
   it "should get tasks changes names" do
     @task_changes = task.get_changes_names({:type_id => [2, 1]})
-    @task_changes[:type_id].first.should == 'Bug'
-    @task_changes[:type_id].last.should == 'Feature'
+    expect(@task_changes[:type_id].first).to eq('Bug')
+    expect(@task_changes[:type_id].last).to eq('Feature')
   end
 
   it "should start to track minutes" do
     task.start_work
-    task.start_time.should_not == nil
+    expect(task.start_time).not_to be_nil
   end
 
   it "should stop to track minutes and get the duration in minutes" do
     task.start_work
     task.stop_work
-    task.start_time.should == nil
-    task.duration_time.should_not == nil
+    expect(task.start_time).to be_nil
+    expect(task.duration_time).not_to be_nil
   end
 
 end
