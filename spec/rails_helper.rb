562cf43813785cc4c668573bb49c8f9c11ec64af
Anna Cruz <anna.cruz@gmail.com>
2018-06-05 10:31:47 -0300

    Adjust rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'shoulda/matchers'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 require 'simplecov'
 SimpleCov.start 'rails'
 
