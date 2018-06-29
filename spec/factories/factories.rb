ab906470ee496ffa77060f4cc7eb361b0fdccd3e
Anna Cruz <anna.cruz@gmail.com>
2018-06-29 18:24:16 -0300

    Add some factories

---
+++ spec/factories/factories.rb
@@ -0,0 +1,51 @@
+FactoryBot.define do
+    sequence :email do |n|
+        "test_mail#{n}@factory.com"
+    end
+
+    factory :user, aliases: [:responsible] do
+        name 'Leonard McCoy'
+        email
+        password 'password'
+    end
+
+    factory :status do
+        name 'ToDo'
+    end
+
+    factory :type do
+        name 'Bug'
+    end
+
+    factory :estimate do
+        name 'Light'
+    end
+
+    factory :project do
+        name 'Project 1'
+        description 'This is the test project'
+        created_at {1.day.ago}
+        updated_at {Time.now}
+        time_track true
+    end
+
+    factory :task do
+        name "Task"
+        description "Example task"
+        user
+        project
+        status
+        type
+        estimate
+        responsible
+        start_time {Time.now}
+    end
+
+    factory :comment do
+        description "Comment"
+        task
+        created_at {Time.now}
+        updated_at  {Time.now}
+        user
+    end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'shoulda/matchers'
+require 'factory_bot'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
@@ -34,12 +35,12 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -62,6 +63,9 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Rails.application.routes.url_helpers
+  config.include Devise::TestHelpers, type: :controller
+
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
