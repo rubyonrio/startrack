3f9bd75270e7488e7c782f91ce57ab14938729cc
Anna Cruz <anna.cruz@gmail.com>
2018-06-29 18:24:48 -0300

    Refactor to use factories instead of fixtures

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
-
 describe Comment do
+  let(:commenter) {build(:user)}
+  let(:task) {build(:task)}
+
   it { should belong_to(:task) }
   it { should belong_to(:user) }
   it { should validate_presence_of(:description) }
@@ -8,13 +10,12 @@ describe Comment do
   it { should validate_presence_of(:user) }
 
   describe "to_format" do
-    let(:comment) { Factory.build(:comment) }
 
-    it "should call to_format before save" do
+    it "format the comments to html" do
       comment = Comment.new
       comment.description = "I got some new issue to resolve!"
-      comment.user = User.find_by_name("Leonard McCoy")
-      comment.task = Task.find_by_name("Room the galaxy")
+      comment.user = commenter
+      comment.task = task
       comment.save
       expect(comment.description).to eq("<p>I got some new issue to resolve!</p>")
     end
