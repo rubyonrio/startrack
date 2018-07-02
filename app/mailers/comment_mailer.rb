24b4e75a9dc4798f259b0611b9c940d6ab790bd8
Anna Cruz <anna.cruz@gmail.com>
2018-07-02 17:28:26 -0300

    Adjust to new rspec syntax
    Also:
     - Fix the use of mailer
     - Use factories instead of fixtures
     - Improvements in how to test

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -5,6 +5,7 @@ class CommentMailer < ActionMailer::Base
     @task = @comment.task
     recipients = ""
     @task.watchers.map { |watcher| recipients << "#{watcher.email}," }
+    recipients << @task.user.email
 
     mail(:to => recipients, :subject => "[#{@comment.task.project.name}] #{@comment.user.name} made a new comment on ##{@comment.task.id} - #{@comment.task.name}", :from => "startrack@example.com.br")
   end
