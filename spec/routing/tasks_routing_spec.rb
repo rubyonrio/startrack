ac1ba8bf8f593684b0a0b5b328b22538f69ce750
Anna Cruz <anna.cruz@gmail.com>
2018-07-06 12:21:33 -0300

    Update rspec expectations

--- spec/routing/tasks_routing_spec.rb
+++ spec/routing/tasks_routing_spec.rb
@@ -1,32 +1,32 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe "routing" do
   it "recognizes and generates #index" do
-    get("/projects/1/tasks").should_not be_routable
+    expect(get("/projects/1/tasks")).not_to be_routable
   end
 
    it "recognizes and generates #create" do
-     post("/projects/1/tasks").should route_to(:controller => "tasks", :action => "create", :project_id => "1")
-     project_tasks_path("1").should == "/projects/1/tasks"
+     expect(post("/projects/1/tasks")).to route_to(:controller => "tasks", :action => "create", :project_id => "1")
+     expect(project_tasks_path("1")).to eq("/projects/1/tasks")
    end
 
    it "recognizes and generates #edit" do
-     get("/projects/1/tasks/1/edit").should route_to(:controller => "tasks", :action => "edit", :project_id => "1", :id => "1")
-     edit_project_task_path("1","1").should == "/projects/1/tasks/1/edit"
+     expect(get("/projects/1/tasks/1/edit")).to route_to(:controller => "tasks", :action => "edit", :project_id => "1", :id => "1")
+     expect(edit_project_task_path("1","1")).to eq("/projects/1/tasks/1/edit")
    end
 
    it "recognizes and generates #update" do
-     put("/projects/1/tasks/1").should route_to(:controller => "tasks", :action => "update", :project_id => "1", :id => "1")
-     project_task_path("1", "1").should == "/projects/1/tasks/1"
+     expect(put("/projects/1/tasks/1")).to route_to(:controller => "tasks", :action => "update", :project_id => "1", :id => "1")
+     expect(project_task_path("1", "1")).to eq("/projects/1/tasks/1")
    end
 
   it "recognizes and generates #destroy" do
-    delete("/projects/1/tasks/1").should route_to(:controller => "tasks", :action => "destroy", :project_id => "1", :id => "1")
-    project_task_path("1", "1").should == "/projects/1/tasks/1"
+    expect(delete("/projects/1/tasks/1")).to route_to(:controller => "tasks", :action => "destroy", :project_id => "1", :id => "1")
+    expect(project_task_path("1", "1")).to eq("/projects/1/tasks/1")
   end
 
   it "recognizes and generates #show" do
-    get("/projects/1/tasks/1").should route_to(:controller => "tasks", :action => "show", :project_id => "1", :id => "1")
-    project_task_path("1", "1").should == "/projects/1/tasks/1"
+    expect(get("/projects/1/tasks/1")).to route_to(:controller => "tasks", :action => "show", :project_id => "1", :id => "1")
+    expect(project_task_path("1", "1")).to eq("/projects/1/tasks/1")
   end
 end
